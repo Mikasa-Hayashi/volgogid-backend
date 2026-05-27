e5dda8cb78a9c784b3681ec4c36cd2d392c66a1d
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-27 19:07:10 +0300

    chore: add mixins

---
+++ app/models/mixins.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class TimestampMixin:
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+
+
+class SoftDeleteMixin:
+    deleted: Mapped[bool] = mapped_column(Boolean, default=False)
