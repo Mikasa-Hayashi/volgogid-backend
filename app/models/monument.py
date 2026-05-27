241909779457f51622dc17512d123a22999e3d8c
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-27 19:14:19 +0300

    chore: remove updated and deleted fields, replaced with mixins

--- app/models/monument.py
+++ app/models/monument.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-
-from sqlalchemy import Boolean, DateTime, Float, Integer, String
+from sqlalchemy import Float, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
+from app.models.mixins import SoftDeleteMixin, TimestampMixin
 
 
-class Monument(Base):
+class Monument(Base, TimestampMixin, SoftDeleteMixin):
     __tablename__ = "monuments"
 
     id: Mapped[str] = mapped_column(String, primary_key=True)
@@ -14,5 +13,3 @@ class Monument(Base):
     lon: Mapped[float] = mapped_column(Float)
     image_url: Mapped[str] = mapped_column(String)
     sort_order: Mapped[int] = mapped_column(Integer)
-    updated_at: Mapped[datetime] = mapped_column(DateTime)
-    deleted: Mapped[bool] = mapped_column(Boolean, default=False)
