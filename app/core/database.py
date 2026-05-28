60211c08c3f5a174350b784ad8110f24c3da849f
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-28 12:35:26 +0300

    chore: add db session dependency

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,20 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     expire_on_commit=False,
 )
+
+
+async def get_db() -> AsyncGenerator[AsyncSession]:
+    async with AsyncSessionLocal() as session:
+        yield session
