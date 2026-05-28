611e08a9aa506a768722280cbb3930bbf00e58bc
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-28 12:16:44 +0300

    refactor: add split lines

--- app/models/route.py
+++ app/models/route.py
@@ -15,7 +15,9 @@ class Route(Base, TimestampMixin, SoftDeleteMixin):
     __tablename__ = "routes"
 
     id: Mapped[str] = mapped_column(String, primary_key=True)
+
     cover_monument_id: Mapped[str] = mapped_column(String)
+
     sort_order: Mapped[int] = mapped_column(Integer)
 
     translations: Mapped[list["RouteTranslation"]] = relationship(
