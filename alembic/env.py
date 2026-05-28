fe2fba22722a88271df5f82d70e7a5cb8604f2cd
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-28 12:44:52 +0300

    chore: add models metadata to alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,11 @@
-from logging.config import fileConfig
+from logging.config import fileConfig  # noqa: I001
 
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+
+from app.models.base import Base
+from app.models import *  # noqa: F403
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
