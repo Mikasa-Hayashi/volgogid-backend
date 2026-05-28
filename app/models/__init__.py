f7e08b6744a04d5a527231f93ba66ce27198eedb
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-28 12:37:46 +0300

    chore: add __init__ for models

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.monument import Monument
+from app.models.monument_translation import MonumentTranslation
+from app.models.route import Route
+from app.models.route_stop import RouteStop
+from app.models.route_translation import RouteTranslation
