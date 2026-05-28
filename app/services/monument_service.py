7bf5a6ad418fdf6983a345133be904b74e3f3851
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-28 18:21:14 +0300

    chore: add get monument by id service

--- app/services/monument_service.py
+++ app/services/monument_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from app.models.monument import Monument
 
@@ -14,3 +15,23 @@ async def get_monuments(db: AsyncSession) -> list[Monument]:
     result = await db.execute(stmt)
 
     return list(result.scalars().all())
+
+
+async def get_monument_by_id(
+    db: AsyncSession,
+    monument_id: str,
+) -> Monument | None:
+    stmt = (
+        select(Monument)
+        .options(
+            selectinload(Monument.translations),
+        )
+        .where(
+            Monument.id == monument_id,
+            Monument.deleted.is_(False),
+        )
+    )
+
+    result = await db.execute(stmt)
+
+    return result.scalar_one_or_none()
