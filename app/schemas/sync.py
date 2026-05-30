200489e3bd8382026dc157662ea3eba4b5223421
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-05-30 14:46:41 +0300

    chore: add route response to sync schema

--- app/schemas/sync.py
+++ app/schemas/sync.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel
 
 from app.schemas.monument import MonumentResponse
 from app.schemas.monument_detail import MonumentTranslationResponse
+from app.schemas.route import RouteResponse
+from app.schemas.route_stop import RouteStopResponse
+from app.schemas.route_translation import RouteTranslationResponse
 
 
 class DeletedIdsResponse(BaseModel):
@@ -11,7 +14,10 @@ class DeletedIdsResponse(BaseModel):
 
 class SyncResponse(BaseModel):
     monuments: list[MonumentResponse]
-
     monument_translations: list[MonumentTranslationResponse]
+
+    routes: list[RouteResponse]
+    route_stops: list[RouteStopResponse]
+    route_translations: list[RouteTranslationResponse]
 
     deleted_ids: DeletedIdsResponse
